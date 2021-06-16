9ae365ece42aa0a2c8142e8ab7d5fe3783fa7c85
Hieu Pham <phamvochihieu@yahoo.com>
2021-06-16 15:10:47 +0700

    update script

--- metal-war.js
+++ metal-war.js
@@ -10,11 +10,22 @@
 
 (function init() {
 
+  const RAID = 1; // Raiding time
+  const REPAIR = 10; // repairing time
+  const MILISECOND = 1000;
+  const SECOND = 60;
+  const DELAY = 2; // delay time for reparing
 
+
+  // Mining - excute every minutes
   setInterval(() => {
     let timer = document.getElementsByClassName('timer')[0];
-    if (!timer) {
-      // Login
+    let timerEl = timer.innerHTML.split(':');
+    if (!timerEl) return;
+    let remainSeconds = 0;
+    remainSeconds = Number(timerEl[0]) * 60 + Number(timerEl[1]);
+    if (!timer || remainSeconds < 30) {
+
       let buttons = document.getElementsByClassName('button raid');
       let raid = null;
       buttons.forEach((item, i) => {
@@ -30,7 +41,33 @@
       } else {
         console.log(new Date().toLocaleString() + ' mining inprogress');
       }
+    } else {
+      console.log(new Date().toLocaleString() + ': ' + timer.outerText + ' - ' + remainSeconds);
     }
-  }, 60000);
+  }, RAID * SECOND * MILISECOND);
+
+  // Repair the tool - excute every 2 minutes
+  setInterval(() => {
+    let hpEl = document.getElementsByClassName('hp_text')[0];
+    if (!hpEl) return;
+    let needRepair = hpEl.innerText.startsWith("0/");
+    if (needRepair) {
+      setTimeout(function() {
+        (function waitMine() {
+          let button = document.getElementsByClassName('repair_price')[0];
+          if (button) {
+            button.click();
+            console.log(new Date().toLocaleString() + ' reparing ...');
+          } else {
+            console.log(new Date().toLocaleString() + 'Error...');
+          }
+        })();
+      }, DELAY * SECOND * MILISECOND);
+
+
+    } else {
+      console.log(new Date().toLocaleString() + ' HP: ' + hpEl.innerText);
+    }
+  }, REPAIR * SECOND * MILISECOND);
 
 })();
